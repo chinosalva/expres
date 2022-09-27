fcd6890fbe39d07313d4a1c1efee6c97bedd7a2c
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-09-26 21:25:51 -0500

    Baby Steps For An API

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express') //Importamos la libreria express
+const app = express()//Inicializamos la libreria en una constante
+const port = 3000//Definimos el puerto
+const router = require('./routes')//Importamos las rutas
+
+//Middlewares --> 
+//Obtienen los datos de la peticion antes de que lleguen a nuestra funcion
+
+app.use(express.json())//Permitimos la comucacion a traves de formato JSON
+app.use(express.urlencoded({extended: false}))//Permitimos recibir datos de codificacion de formularios
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//Utilizamos las rutas
+app.use(router)
+
+
+app.listen(port, () => console.log(`Escuchando en el puerto ${port}!`))
